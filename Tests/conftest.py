bcf9797ae2ddd85789d0be54f3c7aed308576f98
user <sushenbiswasaga@021>
2022-01-19 20:22:51 +0600

    Update : main.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -11,14 +11,14 @@ from Config.config import TestData
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
     if request.param == "chrome":
-        web_driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH)
-        # web_driver = webdriver.Chrome(ChromeDriverManager().install())
+        # web_driver = webdriver.Chrome(executable_path=TestData.CHROME_EXECUTABLE_PATH)
+        web_driver = webdriver.Chrome(ChromeDriverManager().install())
     if request.param == "firefox":
         web_driver = webdriver.Chrome(executable_path=TestData.FIREFOX_EXECUTABLE_PATH)
     request.cls.driver = web_driver
     # 3 We cannot close browser so we comment it
     yield
-    # web_driver.close()
+    web_driver.close()
 
 
 
